27eea2468f73328f6a15580997ab67dd7f294285
Susana Zapari <susy.zapari@gmail.com>
2019-05-10 13:49:46 -0500

    Updates

---
+++ precache-manifest.bebe01bb3f7ffe3067059f0c38b09464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a4b3dcd24433ea004807ae180ff691",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "ef299dc01ef098151530",
+    "url": "/gdl002-burger-queen/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gdl002-burger-queen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ef299dc01ef098151530",
+    "url": "/gdl002-burger-queen/static/js/main.9bfb14fe.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gdl002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
