4f885060a7e5f0089dd0a183b5d6825aa7c8c5a9
Susana Zapari <susy.zapari@gmail.com>
2019-05-28 09:43:39 -0500

    Updates

--- precache-manifest.931f1984ea3dcc56db5699d2b6394d49.js
+++ precache-manifest.931f1984ea3dcc56db5699d2b6394d49.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb786e33a4da7727f381592254fe8136",
+    "revision": "8c58e3413cf4a502dc8572beed7227d4",
     "url": "/gdl002-burger-queen/index.html"
   },
   {
-    "revision": "f71ff1cf01a31abb5225",
+    "revision": "1d4deadd23d75da152af",
     "url": "/gdl002-burger-queen/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "6fea0d8f9b465f863809",
+    "revision": "b4474472908b2852c240",
     "url": "/gdl002-burger-queen/static/css/main.06c03dcc.chunk.css"
   },
   {
-    "revision": "f71ff1cf01a31abb5225",
-    "url": "/gdl002-burger-queen/static/js/2.ecb696f4.chunk.js"
+    "revision": "1d4deadd23d75da152af",
+    "url": "/gdl002-burger-queen/static/js/2.e4c27310.chunk.js"
   },
   {
-    "revision": "6fea0d8f9b465f863809",
-    "url": "/gdl002-burger-queen/static/js/main.98844be0.chunk.js"
+    "revision": "b4474472908b2852c240",
+    "url": "/gdl002-burger-queen/static/js/main.2218853e.chunk.js"
   },
   {
     "revision": "abf1e5c4314e6441e9d2",
