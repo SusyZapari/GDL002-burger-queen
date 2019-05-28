8274f5215a7694963ddc56d5442871f9f2ac87f5
Susana Zapari <susy.zapari@gmail.com>
2019-05-28 13:04:23 -0500

    Updates

---
+++ precache-manifest.cec710e816886b5f9cd3bd7d00e01ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c549655b15126f1f9c6d613e1d591f4b",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "f71ff1cf01a31abb5225",
+    "url": "/gdl002-burger-queen/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "93a49ce098d0913f2079",
+    "url": "/gdl002-burger-queen/static/css/main.06c03dcc.chunk.css"
+  },
+  {
+    "revision": "f71ff1cf01a31abb5225",
+    "url": "/gdl002-burger-queen/static/js/2.ecb696f4.chunk.js"
+  },
+  {
+    "revision": "93a49ce098d0913f2079",
+    "url": "/gdl002-burger-queen/static/js/main.94f7bf54.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GDL002-burger-queen/precache-manifest.d4d81b73fed0056873afef2d003d6ed5.js"
+  "/gdl002-burger-queen/precache-manifest.cec710e816886b5f9cd3bd7d00e01ca8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GDL002-burger-queen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gdl002-burger-queen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
